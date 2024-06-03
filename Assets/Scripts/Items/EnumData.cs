a387582ea7a4d2794eb53401e7c2c68d8c807b96
YongJeaWoo <subrosastar97@gmail.com>
2024-06-04 01:47:47 +0900

    fix Change character curse effect code refactor
    test buffdamage character

--- Assets/Scripts/Items/EnumData.cs
+++ Assets/Scripts/Items/EnumData.cs
@@ -17,7 +17,8 @@ public class EnumData : MonoBehaviour
 
     public enum E_ConsumerType
     {
-        HpUp,       // 체력 증가
-        Curse,      // 체력 감소 (저주)
+        HpUp,           // 체력 증가
+        Curse,          // 체력 감소 (저주)
+        BuffAttack  // 공격력 증가
     }
 }
